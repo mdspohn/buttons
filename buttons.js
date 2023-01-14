47813ccb1fc32009448f895fe71d8c48aea269d3
M <4waltz@gmail.com>
2023-01-14 02:56:03 -0500

    add shine button

--- buttons.js
+++ buttons.js
@@ -46,51 +46,59 @@ class StandardButton extends Button {
     }
 }
 
-class FloodButton extends Button {
+class WaveButton extends Button {
     constructor(element) {
         super(element);
         element.style.overflow = 'hidden';
     }
 
     onClick(e) {
-        if (!this.element.classList.contains('selected')) {
-            const x = e.target.offsetLeft + e.offsetX;
-            const y = e.target.offsetTop + e.offsetY;
-            const wave = document.createElement('div');
-            wave.style.display = 'block';
-            wave.style.position = 'absolute';
-            wave.style.height = 0;
-            wave.style.width = 0;
-            wave.style.borderRadius = '100%';
+        const x = e.target.offsetLeft + e.offsetX;
+        const y = e.target.offsetTop + e.offsetY;
+        const wave = document.createElement('div');
+        wave.style.display = 'block';
+        wave.style.position = 'absolute';
+        wave.style.height = 0;
+        wave.style.width = 0;
+        wave.style.borderRadius = '100%';
 
-            const color = [...this.element.classList].find(c => ['blue', 'green', 'red'].includes(c));
-            wave.style.backgroundColor = `var(--lighter-${color})`;
-            wave.style.transition = 'width 800ms ease-in-out, height 800ms ease-in-out, background-color 700ms ease-in-out';
-            
-            this.element.prepend(wave);
-    
-            setTimeout(() => {
-                wave.style.top = y + 'px';
-                wave.style.left = x + 'px';
-                wave.style.transform = 'translate(-50%, -50%)';
-                wave.style.height = '600px';
-                wave.style.width = '600px';
-                wave.style.backgroundColor = 'transparent';
-            }, 0);
-    
-            setTimeout(() => {
-                wave.remove();
-            }, 800)
-        }
+        const color = [...this.element.classList].find(c => ['blue', 'green', 'red'].includes(c));
+        wave.style.backgroundColor = `var(--lighter-${color})`;
+        wave.style.transition = 'width 800ms ease-in-out, height 800ms ease-in-out, background-color 700ms ease-in-out';
+        
+        this.element.prepend(wave);
+
+        setTimeout(() => {
+            wave.style.top = y + 'px';
+            wave.style.left = x + 'px';
+            wave.style.transform = 'translate(-50%, -50%)';
+            wave.style.height = '600px';
+            wave.style.width = '600px';
+            wave.style.backgroundColor = 'transparent';
+        }, 0);
+
+        setTimeout(() => {
+            wave.remove();
+        }, 800);
     }
 }
 
-class WaveButton extends Button {
+class ShineButton extends Button {
     constructor(element) {
         super(element);
+        element.style.overflow = 'hidden';
+        this.timeout = undefined;
     }
 
     onClick(e) {
-        this.element.classList.toggle('selected');
+        this.element.classList.toggle('selected', false);
+        clearTimeout(this.timeout);
+        setTimeout(() => {
+            this.element.classList.toggle('selected', true);
+        }, 0);
+
+        this.timeout = setTimeout(() => {
+            this.element.classList.toggle('selected', false);
+        }, 800);
     }
 }
