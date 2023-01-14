d6a1ac5f459f65851920208c8eecbd6c51bba9b3
M <4waltz@gmail.com>
2023-01-13 23:30:02 -0500

    add standard button type

--- buttons.js
+++ buttons.js
@@ -1,9 +1,54 @@
 'use strict';
 
-class BC {
+class Button {
     constructor(element) {
         this.element = element;
         this.element.addEventListener('click', this.onClick.bind(this));
+        this.element.addEventListener('contextmenu', this.onRightClick.bind(this));
+    }
+
+    onClick(e) {
+        // Abstract
+    }
+
+    onRightClick(e) {
+        // Abstract
+    }
+}
+
+class CoursewareButton extends Button {
+    constructor(element) {
+        super(element);
+    }
+
+    onClick(e) {
+        this.element.classList.toggle('selected');
+    }
+
+    onRightClick(e) {
+        this.element.classList.toggle('disabled');
+        e.preventDefault();
+    }
+}
+
+class StandardButton extends Button {
+    constructor(element) {
+        super(element);
+    }
+
+    onClick(e) {
+        this.element.classList.toggle('selected');
+    }
+
+    onRightClick(e) {
+        this.element.classList.toggle('disabled');
+        e.preventDefault();
+    }
+}
+
+class FloodButton extends Button {
+    constructor(element) {
+        super(element);
     }
 
     onClick(e) {
@@ -11,8 +56,12 @@ class BC {
     }
 }
 
-class Flood {
-    constructor(elem) {
-        this.elem = elem;
+class WaveButton extends Button {
+    constructor(element) {
+        super(element);
+    }
+
+    onClick(e) {
+        this.element.classList.toggle('selected');
     }
 }
